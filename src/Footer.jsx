481b66200829ab59beeb639a3d358eab4a554226
Maartenhollander <maarten.hollander@student.hu.nl>
2026-03-03 12:41:34 +0100

    Componenten aangemaakt

---
+++ src/Footer.jsx
@@ -0,0 +1,26 @@
+import './app.css';
+
+export const footer = () => {
+    return (<footer>
+        <div>
+            <ul>
+                <li> <a href="#">Kaart</a></li>
+                <li> <a href="#">onze kuntenaars</a></li>
+            </ul>
+
+            <ul>
+                <li> <a href="#">Kaart</a></li>
+                <li> <a href="#">onze kuntenaars</a></li>
+            </ul>
+            <ul>
+                <p>© Alle rechten zijn voorbehouden</p>
+                <p>Vindt hier de gebruikersvoorwaarden</p>
+            </ul>
+            
+        </div>
+
+
+    </footer>)
+
+
+}
