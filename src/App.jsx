b4ca4a284f76e204ae4363f93e5fd6f258eb71c2
Maartenhollander <maarten.hollander@student.hu.nl>
2026-03-03 11:11:07 +0100

    statische kaarten met dummy data

--- src/App.jsx
+++ src/App.jsx
@@ -43,9 +43,9 @@ const App = () => (
     />
     <Card
       picture="https://picsum.photos/seed/janisdeman/320/320"
-      name="JanIsDeMan"
+      name="Jan Is De Man"
       title="Street Art Kunstenaar"
-      bio="Creeërt muurschilderingen verbonden met de locatie, met iconische ogen en boekenkasten. Utrechtse graffiti-artiest."
+      bio="Creëert muurschilderingen die verbonden zijn met de locatie, vaak met iconische ogen en boekenkasten."
       adres="Oudegracht 200, Utrecht"
       wheelchairaccessibility="Afhankelijk van locatie"
       day="Zondag"
@@ -53,7 +53,42 @@ const App = () => (
       email="info@janisdeman.com"
       website="https://www.janisdeman.com"
     />
-    
+    <Card
+      picture="https://picsum.photos/seed/emilyart/320/320"
+      name="Emily van den Broek"
+      title="Keramiste"
+      bio="Werkt met lokale klei om natuurlijke texturen te verkennen in minimalistische keramiek."
+      adres="Lombokstraat 12, Utrecht"
+      wheelchairaccessibility="Ja"
+      day="Donderdag & Vrijdag"
+      phonenumer="06-11223344"
+      email="studio@emilykeramiek.nl"
+      website="https://www.emilykeramiek.nl"
+    />
+    <Card
+      picture="https://picsum.photos/seed/lucaschreuder/320/320"
+      name="Lucas Schreuder"
+      title="Fotograaf"
+      bio="Richt zich op stedelijke architectuur en het samenspel tussen licht en schaduw."
+      adres="Wittevrouwenstraat 45, Utrecht"
+      wheelchairaccessibility="Nee"
+      day="Woensdag"
+      phonenumer="030-9988776"
+      email="lucas@lichtbeeld.com"
+      website="https://www.lichtbeeld.com"
+    />
+    <Card
+      picture="https://picsum.photos/seed/sofiedesigns/320/320"
+      name="Sofie Designs"
+      title="Grafisch Ontwerper"
+      bio="Combineert typografie en krachtige kleuren in branding voor duurzame merken."
+      adres="Kanaalstraat 88, Utrecht"
+      wheelchairaccessibility="Ja"
+      day="Maandag t/m Vrijdag"
+      phonenumer="030-2233445"
+      email="sofie@sofiedesigns.nl"
+      website="https://www.sofiedesigns.nl"
+    />
   </div>
 );
 
