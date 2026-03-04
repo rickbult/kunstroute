92e06b91d5eb6ce4d974cfdda796c614153fd343
JabirGh <jabir.ghalit@student.hu.nl>
2026-03-04 10:45:11 +0100

    Bestanden overgezet

---
+++ Kunstrouteapp/src/Navbar.jsx
@@ -0,0 +1,24 @@
+export default function Navbar() {
+  return (
+    <nav className="navbar">
+      <a href="/" className="site-title">Kunst Route</a>
+      
+      <ul>
+        <li className="active">
+          <a href="#">Kaart</a>
+        </li>
+        <li className="active">
+          <a href="#">Kunstwerken</a>
+        </li>
+        <li className="active">
+          <a href="#">Kunstenaars</a>
+        </li>
+        <li className="active">
+          <a href="#">Info & Agenda</a>
+        </li>
+         <li className="active"></li>
+         <a href="#">Inschrijven</a>
+      </ul>
+    </nav>
+  );
+}
