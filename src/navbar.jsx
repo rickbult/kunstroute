bf2047d079c3d372287ed1121c6c0d6321736449
Maartenhollander <maarten.hollander@student.hu.nl>
2026-03-06 13:35:42 +0100

    emoji's

--- src/navbar.jsx
+++ src/navbar.jsx
@@ -1,13 +1,13 @@
 export default function Navbar() {
   return (
     <nav className="navbar">
-      <a href="/" className="site-title">Kunst Route</a>
+      <a href="/" className="site-title">🎨 Kunst Route</a>
       <ul>
-        <li className="active"><a href="#">Kaart</a></li>
-        <li className="active"><a href="#">Kunstwerken</a></li>
-        <li className="active"><a href="#">Kunstenaars</a></li>
-        <li className="active"><a href="#">Info & Agenda</a></li>
-        <li className="active"><a href="#">Inschrijven</a></li>
+        <li className="active"><a href="#">🗺️ Kaart</a></li>
+        <li className="active"><a href="#">🎨 Kunstwerken</a></li>
+        <li className="active"><a href="#">👤 Kunstenaars</a></li>
+        <li className="active"><a href="#">ℹ️ Info & Agenda</a></li>
+        <li className="active"><a href="#">📝 Inschrijven</a></li>
       </ul>
     </nav>
   );
