def3328d9ccbd0244ce74aa4c6dbc73c254dfd1d
Maartenhollander <maarten.hollander@student.hu.nl>
2026-03-06 13:48:01 +0100

    logo en achtergrond navbar

--- src/navbar.jsx
+++ src/navbar.jsx
@@ -1,7 +1,12 @@
 export default function Navbar() {
   return (
     <nav className="navbar">
-      <a href="/" className="site-title">🎨 Kunst Route</a>
+      <a href="/" className="logo">
+        <img src="/src/assets/kunstroutelogo.png" 
+          alt="Kunst Route Noordwest Veluwe" 
+          className="logo-img"
+        />
+      </a>
       <ul>
         <li className="active"><a href="#">🗺️ Kaart</a></li>
         <li className="active"><a href="#">🎨 Kunstwerken</a></li>
