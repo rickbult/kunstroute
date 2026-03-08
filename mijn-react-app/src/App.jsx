270bf26cdf93d916cc909d90a5d4deebc7445974
Rick Bult <technobult@gmail.com>
2026-03-08 15:22:42 +0100

    https://youtu.be/5RSImGMZzLk is done.

--- mijn-react-app/src/App.jsx
+++ mijn-react-app/src/App.jsx
@@ -15,6 +15,18 @@ export function App() {
       name: "Mark",
       title: "Frontend developer",
       bio: "I like to work with different frontend technologies and play video games."
+    },
+    {
+      id: 2,
+      name: "Tiffany",
+      title: "Engineering manager",
+      bio: "I have worked in tech for 15 years and love to help people grow in this industry.",
+    },
+    {
+      id: 3,
+      name: "Doug",
+      title: "Backend developer",
+      bio:  "I have been a software developer for over 20 years and I love working with Go and Rust.",
     }
   ]
 
