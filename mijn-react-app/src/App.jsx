823c0960ef00f4196d83fc1116fb8d1eb38870c5
Rick Bult <technobult@gmail.com>
2026-03-08 15:10:01 +0100

    Can't wait for the tutorial to be over...

--- mijn-react-app/src/App.jsx
+++ mijn-react-app/src/App.jsx
@@ -9,23 +9,25 @@ export function Card({name, title, bio}) {
 } 
 
 export function App() {
+  const profiles = [
+    {
+      id: 1,
+      name: "Mark",
+      title: "Frontend developer",
+      bio: "I like to work with different frontend technologies and play video games."
+    }
+  ]
+
   return (
     <div className="flex-container">
-      <Card
-      name="Mark"
-      title="Frontend developer"
-      bio="Een kilo kaas is de gemiddelde hoeveelheid kaas die ik per dag eet. Sommige mensen vinden het raar dat ik zo veel kaas eet, maar ik niet."
-      />
-      <Card
-      name="Tiffany"
-      title="Engineering manager"
-      bio="I have worked in tech for 15 years and love to help people grow in this industry."
-      />
-      <Card
-      name="Doug"
-      title="Backend developer"
-      bio="I have been a software developer for over 20 years and I love working with Go and Rust."
-      />
+      {profiles.map(profile => (
+        <Card
+          key={profile.id}
+          name={profile.name}
+          title={profile.title}
+          bio={profile.bio}
+        />
+      ))}
     </div>
   )
 }
