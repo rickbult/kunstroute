c3faf6e88d8ded5a6d4b179b65018f480788e970
JabirGh <jabir.ghalit@student.hu.nl>
2026-03-17 17:04:13 +0100

    verandering style.css naar styles.css en logo toegevoegd aan navbar

--- kunst-route/src/index.jsx
+++ kunst-route/src/index.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDom from "react-dom/client"
 import { BrowserRouter } from "react-router-dom"
 import App from "./App";
-import "./style.css"
+import "./styles.css"
 
 const root = ReactDom.createRoot(document.getElementById("root"))
 root.render(
