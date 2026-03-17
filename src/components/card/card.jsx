23576296cc6f311d04e9075594e8be92fdd0a8c0
Maartenhollander <maarten.hollander@student.hu.nl>
2026-03-17 10:35:37 +0100

    app test

--- src/components/card/card.jsx
+++ src/components/card/card.jsx
@@ -2,26 +2,18 @@ import React from "react";
 import { Link } from "react-router-dom";
 import "./Card.css";
 
-export const Card = ({
-  imgSrc,
-  imgAlt,
-  title = "Naam kunstenaar",
-  location = "Locatie",
-  description = "Korte beschrijving",
-  days = "Dagen Aanwezig",
-  link = "#",
-}) => {
+export const Card = ({ artist }) => {
   return (
-    <Link to={`/artist/${link}`} className="card-link">
+    <Link to={`/artist/${artist.id}`} className="card-link">
       <div className="card-container">
         <div className="card-header">
-          {imgSrc && imgAlt && <img src={imgSrc} alt={imgAlt} />}
-          {title && <h1 className="card-title">{title}</h1>}
-          {location && <div className="card-location"> {location}</div>}
+          <img src={artist.picture} alt={artist.name} />
+          <h1 className="card-title">{artist.name}</h1>
+          <div className="card-location">{artist.adres}</div>
         </div>
         <div className="card-body">
-          {description && <p className="card-description">{description}</p>}
-          {days && <span className="card-btn">{days}</span>}
+          <p className="card-description">{artist.bio}</p>
+          <span className="card-btn">{artist.day}</span>
         </div>
       </div>
     </Link>
