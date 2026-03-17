8b8ab2098aab2c7ef37b1c77afc066288cb491d2
Maartenhollander <maarten.hollander@student.hu.nl>
2026-03-17 12:27:06 +0100

    install

---
+++ src/searchbar/searchbar.jsx
@@ -0,0 +1,9 @@
+import "./searchbar.css";
+
+export default function Searchbar() {
+  return (
+    <div className="searchbar">
+      <input type="text" placeholder="🔍 Zoek kunstenaars, locaties..." />
+    </div>
+  );
+}
