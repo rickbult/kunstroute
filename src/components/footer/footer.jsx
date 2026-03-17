f37262b50db96cba655b773397b47dc8af6a6a11
Maartenhollander <maarten.hollander@student.hu.nl>
2026-03-17 10:25:45 +0100

    gemerged (handmatig)

---
+++ src/components/footer/footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './footer.css';  
+
+export default function Footer() {
+  return (
+    <footer className="footer">
+      <p>&copy; 2026 Kunstroute Noord West - Veluwe</p>
+      <p>Door de Picassoboys | Hogeschool Utrecht</p>
+    </footer>
+  );
+}
