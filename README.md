6ea92fba67036bbcafcda33aac44a60e1e82329b
Maartenhollander <maarten.hollander@student.hu.nl>
2026-03-17 15:04:20 +0100

    gefixt

---
+++ README.md
@@ -0,0 +1,7 @@
+# react-card
+Cash legen:
+cd ..                    # ← NAAR /kunstroute/
+npm cache clean --force
+rm -rf node_modules package-lock.json
+npm install
+npm run dev
---
+++ src/components/Footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Footer.css';
+
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <p>Kunstroute Noord West - Veluwe © 2026</p>
+    </footer>
+  );
+};
+
+export default Footer;
