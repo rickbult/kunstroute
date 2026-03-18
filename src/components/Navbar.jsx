2f1413eae3bfabe244e1940d9dab619b39bf8ce6
Maartenhollander <maarten.hollander@student.hu.nl>
2026-03-18 10:23:26 +0100

    navbar gefixt

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,24 +1,25 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import "./Navbar.css";
+import logo from "/src/assets/kunstroutelogo.png";
 
 export default function Navbar() {
   return (
     <nav className="navbar">
       <Link to="/" className="logo">
-        <img 
-          src="/src/assets/kunstroutelogo.png" 
-          alt="Kunst Route Noordwest Veluwe" 
-          className="logo-img"
-        />
+        <img src={logo} alt="Kunst Route Noordwest Veluwe" className="logo-img" />
       </Link>
-      <ul>
-        <li><Link to="/">🗺️ Kaart</Link></li>
-        <li><Link to="/">🎨 Kunstwerken</Link></li>
-        <li><Link to="/">👤 Kunstenaars</Link></li>
-        <li><Link to="/">ℹ️ Info & Agenda</Link></li>
-        <li><Link to="/">📝 Inschrijven</Link></li>
+
+      <ul className="nav-links">
+        <li><Link to="/kaart" className="tekst-groen">Kaart</Link></li>
+        <li><Link to="/kunstwerken" className="tekst-roze">Kunstwerken</Link></li>
+        <li><Link to="/kunstenaars" className="tekst-geel">Kunstenaars</Link></li>
+        <li><Link to="/info-agenda" className="tekst-blauw">Info & Agenda</Link></li>
+        <li>
+          <Link to="/inschrijven" className="btn-inschrijven">Inschrijven</Link>
+        </li>
       </ul>
     </nav>
   );
-}z
+}
+
