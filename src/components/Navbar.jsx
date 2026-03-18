6fcbd32011405061b4a4a360384a0272e68ca1ce
Maartenhollander <maarten.hollander@student.hu.nl>
2026-03-18 12:16:09 +0100

    card fix

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -11,12 +11,12 @@ export default function Navbar() {
       </Link>
 
       <ul className="nav-links">
-        <li><Link to="/src/pages/map.jsx" className="tekst-groen">Kaart</Link></li>
+        <li><Link to="/src/pages/Map.jsx" className="tekst-groen">Kaart</Link></li>
         <li><Link to="/src/pages/Artwork.jsx" className="tekst-roze">Kunstwerken</Link></li>
         <li><Link to="/src/pages/Artists.jsx" className="tekst-geel">Kunstenaars</Link></li>
         <li><Link to="/src/pages/Info-agenda.jsx" className="tekst-blauw">Info & Agenda</Link></li>
         <li>
-          <Link to="/inschrijven" className="btn-inschrijven">Inschrijven</Link>
+          <Link to="/src/pages/Signup.jsx" className="btn-inschrijven">Inschrijven</Link>
         </li>
       </ul>
     </nav>
