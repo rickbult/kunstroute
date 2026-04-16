9d7c7aaf383c4d50df977089102e37a71566162a
Rick Bult <technobult@gmail.com>
2026-04-16 12:18:40 +0200

    mijn eerste marker toegevoegd!

--- src/components/ui/map.jsx
+++ src/components/ui/map.jsx
@@ -1,5 +1,17 @@
-function Map() {
-  return <div>Hier kommt mijn epische interactieve kaart!</div>;
-}
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
 
-export default Map;
+export default function Map() {
+  return (
+  <MapContainer center={[52.0907, 5.1214]} zoom={13} style={{ height: '400px' }}>
+    <TileLayer
+      url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      attribution="&copy; OpenStreetMap contributors"
+    />
+    <Marker position={[52.0907, 5.1214]}>
+      <Popup>
+        Dit is mijn eerste marker!
+      </Popup>
+    </Marker>
+  </MapContainer>
+);
+}
