f8eb3801c5884b006e0429467fcf49a025b9d19d
Rick Bult <technobult@gmail.com>
2026-05-11 23:45:24 +0200

    I should sleep hihi

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -11,7 +11,12 @@ export default function Navbar() {
       </Link>
 
       <ul className="nav-links">
-        <li><Link to="/kaart" className="tekst-groen">Kaart</Link></li>
+        <li>
+          <Link to="/kaart" className="tekst-groen">
+            Route
+            <span className="badge-nieuw">NIEUW!</span>
+          </Link>
+        </li>
         <li><Link to="/kunstwerken" className="tekst-roze">Kunstwerken</Link></li>
         <li><Link to="/kunstenaars" className="tekst-geel">Kunstenaars</Link></li>
         <li><Link to="/info" className="tekst-blauw">Info</Link></li>
